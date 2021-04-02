190ebbf1d351970f6fc431dfef6111135840a64a
niekcandaele <niek@candaele.dev>
2021-04-02 09:49:19 +0200

    fix: use exec instead of eval

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,4 @@ MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+exec ${MODIFIED_STARTUP}
